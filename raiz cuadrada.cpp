2bea3a9fe123479e1104f62f04c225079b364851
CesarVillagomez117 <viej080310hgtlssa6@cbtis217.edu.mx>
2024-10-15 08:28:59 -0600

    agg comentarios en el codigo

--- raiz cuadrada.cpp
+++ raiz cuadrada.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 int main (){
     double a, b, c;
+    //valores que vas a utilizar
     cout<<" introduce el valor de a"<< endl;
     cin>>a;
     cout << "introduce el valor de b"<< endl;
     cin >> b;
     cout << "introduce el valor de c"<< endl;
     cin >>c;
+    //el resulatdo
     cout << "resultado...";
       cout<<"x1... "<<setprecision(3)<<((b*-1)+sqrt((b*b)-((a*c)*4)))/(a*2)<<endl;
     cout<<"x2... "<<setprecision(3)<<((b*-1)-sqrt((b*b)-((a*c)*4)))/(a*2)<<endl;
